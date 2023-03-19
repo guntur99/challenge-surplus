6ef3a4e0e4d7b4b7bf7e16b11dc0577ccf0d28d9
Guntur <gugunguntur99@gmail.com>
2023-03-19 18:26:41 +0700

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\ImageController;
 
 /*
 |--------------------------------------------------------------------------
